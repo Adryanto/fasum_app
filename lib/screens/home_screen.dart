45a041be47cfd98c0bb5bc380d5cd7b1e5f0637a
Adryanto <yantobae112233@mhs.mdp.ac.id>
2024-05-17 19:32:06 +0700

    tampilan ke cardview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fasum_app/screens/sign_in_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -25,41 +26,95 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    final email = user?.email;
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home'),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  final post = snapshot.data!.docs[index];
-                  return ListTile(
-                    title: Text(post['text']),
-                    leading: Image.network(post['image_url']),
-                  );
-                },
-              );
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          backgroundColor: Color.fromARGB(255, 129, 214, 248),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) =>  PostScreen()),
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              bool isSignedOut = await signOut(context);
+              if (isSignedOut) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Signed out successfully')),
+                );
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Failed to sign out')),
+                );
+              }
+            },
+          ),
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final post = snapshot.data!.docs[index];
+                final timestamp = post['timestamp']; // assume 'timestamp' is the field name in Firestore
+                final date = timestamp.toDate(); // convert Timestamp to DateTime
+                final formattedDate = DateFormat.yMMMd().format(date); // format the date
+                final formattedTime = DateFormat.jm().format(date); // format the time
+
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(18.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(post['text']),
+                                Text('Tanggal = $formattedDate'),  // display the formatted date
+                                Text('Jam = $formattedTime'),  // display the formatted time
+                                Text('Email = $email'),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: Image.network(post['image_url']),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             );
-          },
-          label: const Text('Add'),
-          icon: const Icon(Icons.add,
-              color: Color.fromARGB(255, 3, 3, 3), size: 25),
-        ));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Color.fromARGB(255, 129, 214, 248),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PostScreen()),
+          );
+        },
+        label: const Text('Add'),
+        icon: const Icon(Icons.add,
+            color: Color.fromARGB(255, 3, 3, 3), size: 25),
+      ),
+    );
   }
 }
--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -1,146 +1,148 @@
-import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+  import 'dart:io';
+  import 'package:cloud_firestore/cloud_firestore.dart';
+  import 'package:firebase_storage/firebase_storage.dart';
+  import 'package:flutter/material.dart';
+  import 'package:image_picker/image_picker.dart';
 
-class PostScreen extends StatefulWidget {
-  @override
-  _PostScreenState createState() => _PostScreenState();
-}
+  class PostScreen extends StatefulWidget {
+    @override
+    _PostScreenState createState() => _PostScreenState();
+  }
 
-class _PostScreenState extends State<PostScreen> {
-  final _postTextController = TextEditingController();
-  final ImagePicker _picker = ImagePicker();
-  XFile? _image;
+  class _PostScreenState extends State<PostScreen> {
+    final _postTextController = TextEditingController();
+    final ImagePicker _picker = ImagePicker();
+    XFile? _image;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Tambah Postingan'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            GestureDetector(
-              onTap: () async {
-                await _showImageSourceDialog();
-              },
-              child: Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text('Tambah Postingan'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
 
+              GestureDetector(
+                onTap: () async {
+                  await _showImageSourceDialog();
+                },
+
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+
+                  ),
+                  child: _image != null
+                      ? Image.file(File(_image!.path))
+                      : Icon(Icons.camera_alt),
                 ),
-                child: _image != null
-                    ? Image.file(File(_image!.path))
-                    : Icon(Icons.camera_alt),
               ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _postTextController,
-              decoration: InputDecoration(
-                hintText: 'Masukkan Deskripsi',
-                border: OutlineInputBorder(),
+              SizedBox(height: 16),
+              TextField(
+                controller: _postTextController,
+                decoration: InputDecoration(
+                  hintText: 'Masukkan Deskripsi',
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
+              SizedBox(height: 16),
 
-            ElevatedButton(
-              onPressed: () async {
-                if (_image == null) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Please select an image')),
-                  );
-                  return;
-                }
+              ElevatedButton(
+                onPressed: () async {
+                  if (_image == null) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Please select an image')),
+                    );
+                    return;
+                  }
 
-                Reference referenceRoot = FirebaseStorage.instance.ref();
-                Reference referenceDirImages = referenceRoot.child("images");
-                Reference referenceImagesToUpload =
-                referenceDirImages.child(_image!.path.split("/").last);
+                  Reference referenceRoot = FirebaseStorage.instance.ref();
+                  Reference referenceDirImages = referenceRoot.child("images");
+                  Reference referenceImagesToUpload =
+                  referenceDirImages.child(_image!.path.split("/").last);
 
-                try {
-                  final uploadTask =
-                  await referenceImagesToUpload.putFile(File(_image!.path));
-                  final downloadUrl = await uploadTask.ref.getDownloadURL();
+                  try {
+                    final uploadTask =
+                    await referenceImagesToUpload.putFile(File(_image!.path));
+                    final downloadUrl = await uploadTask.ref.getDownloadURL();
 
-                  // Add Firebase Cloud Firestore functionality here
-                  final CollectionReference posts =
-                  FirebaseFirestore.instance.collection('posts');
-                  await posts.add({
-                    'text': _postTextController.text,
-                    'image_url': downloadUrl,
-                    'timestamp': Timestamp.now(),
-                  });
+                    // Add Firebase Cloud Firestore functionality here
+                    final CollectionReference posts =
+                    FirebaseFirestore.instance.collection('posts');
+                    await posts.add({
+                      'text': _postTextController.text,
+                      'image_url': downloadUrl,
+                      'timestamp': Timestamp.now(),
+                    });
 
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Image uploaded successfully')),
-                  );
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Image uploaded successfully')),
+                    );
 
-                  Navigator.pop(context);
-                } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Error uploading image: $e')),
-                  );
-                }
-              },
-              child: Text('Post'),
-            ),
-          ],
+                    Navigator.pop(context);
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Error uploading image: $e')),
+                    );
+                  }
+                },
+                child: Text('Post'),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
+    }
 
-  Future<void> _showImageSourceDialog() async {
-    await showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Choose Image Source'),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ListTile(
-              title: Text('Take a new photo'),
-              onTap: () async {
-                Navigator.of(context).pop();
-                final pickedFile =
-                await _picker.pickImage(source: ImageSource.camera);
-                if (pickedFile != null) {
-                  setState(() {
-                    _image = pickedFile;
-                  });
-                }
-              },
-            ),
-            ListTile(
-              title: Text('Choose from library'),
-              onTap: () async {
-                Navigator.of(context).pop();
-                final pickedFile =
-                await _picker.pickImage(source: ImageSource.gallery);
-                if (pickedFile != null) {
-                  setState(() {
-                    _image = pickedFile;
-                  });
-                }
-              },
-            ),
-          ],
+    Future<void> _showImageSourceDialog() async {
+      await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Choose Image Source'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text('Take a new photo'),
+                onTap: () async {
+                  Navigator.of(context).pop();
+                  final pickedFile =
+                  await _picker.pickImage(source: ImageSource.camera);
+                  if (pickedFile != null) {
+                    setState(() {
+                      _image = pickedFile;
+                    });
+                  }
+                },
+              ),
+              ListTile(
+                title: Text('Choose from library'),
+                onTap: () async {
+                  Navigator.of(context).pop();
+                  final pickedFile =
+                  await _picker.pickImage(source: ImageSource.gallery);
+                  if (pickedFile != null) {
+                    setState(() {
+                      _image = pickedFile;
+                    });
+                  }
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
+    }
 
-  @override
-  void dispose() {
-    _postTextController.dispose();
-    super.dispose();
+    @override
+    void dispose() {
+      _postTextController.dispose();
+      super.dispose();
+    }
   }
-}
